9f1cdc4b174d673cb8cd923a7c18fcce346de8b2
jacheg <jacchegia@gmail.com>
2020-01-23 02:27:47 +0000

    Added objects, good luck with refactoring! :)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 var songs = ["Erase Me", "Ghost Town", "444 + 222"];
+let eraseMe = {
+  imageURL: "https://upload.wikimedia.org/wikipedia/en/0/0a/Kidcudimanonthemoonthelegendof.jpg",
+  artist: "Kid Cudi",
+  length: 312,
+  songLink: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=2ahUKEwiakJz_gIfnAhVYjp4KHSgrA7kQ3ywwAHoECAYQAw&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DICaTsTkBPV8&usg=AOvVaw2IZ-7-HlRpGmHbiGJ74YDe"
+}
+
+let ghostTown = {
+  imageURL: "https://e.snmc.io/i/300/w/47a3a3594b2215b8f09bea95c8699c67/7025219",
+  artist: "Kanye West",
+  length: 312,
+  songLink: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=2ahUKEwj278-NgYfnAhUTj54KHah8Cm4QuAIwAHoECA8QBg&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DqAsHVwl-MU4&usg=AOvVaw09RZpnJ-2bA5m5v_IbHizi"
+}
+
+let vert = {
+  imageURL: "https://images.genius.com/1774302a38314b526f5c44446e40c3ab.1000x1000x1.jpg",
+  artist: "Lil Uzi Vert",
+  length: 407,
+  songLink: "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=2ahUKEwiKhp2XgYfnAhWTpp4KHTyuCWoQ3ywwAHoECAYQAw&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DEepkVbNveX0&usg=AOvVaw1YUgqJBjkAZg3PToBRr7wc"
+}
